2ecf4f3a53b576ea7cb95e1812e2ecc19f8dd209
Phillip Tran <phillip.tran@ldcom.com>
2016-11-07 16:07:25 -0600

    Added route back to MetadataController

--- vNext/src/Microsoft.AspNetCore.OData/MetadataController.cs
+++ vNext/src/Microsoft.AspNetCore.OData/MetadataController.cs
@@ -11,7 +11,7 @@ using System.Linq;
 namespace Microsoft.AspNetCore.OData
 {
 	[ApiExplorerSettings(IgnoreApi = true)]
-	//[Route("odata")]
+	[Route("odata")]
 	public class MetadataController : Controller
 	{
 		private readonly IEdmModel _model;
